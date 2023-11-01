3e16c109cd9ce2936ed60ba6f5d78f4b6299a391
Vasipalli <83403698+eshangonemad@users.noreply.github.com>
2023-11-01 22:11:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function _extends() {_extends = Object.assign || function (target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i];for (var key in source) {if (Object.prototype.hasOwnProperty.call(source, key)) {target[key] = source[key];}}}return target;};return _extends.apply(this, arguments);}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const PointTarget = ReactPoint.PointTarget;
-
+ 
 class AutoScalingText extends React.Component {constructor(...args) {super(...args);_defineProperty(this, "state",
     {
       scale: 1 });}
